8648ecf25b8a42560c150eb3fcea18a940800273
Alex Glaubitz <e.pirazzi@gmail.com>
2022-03-15 17:24:56 -0400

    data saves

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,51 @@
 const textareaEls = $('textarea');
+const containerEl = $('.container');
+const saveBtnEls = $('.saveBtn');
 
 //display current date in header
-const currentDay = moment().local().format('dddd, MMMM Do')
-$('#currentDay').text(currentDay)
+const currentDay = moment().local().format('dddd, MMMM Do');
+$('#currentDay').text(currentDay);
 
+let hourData = [
+  {
+    time: '9AM',
+    text: ''
+  },
+  {
+    time: '10AM',
+    text: ''
+  },
+  {
+    time: '11AM',
+    text: ''
+  },
+  {
+    time: '12PM',
+    text: ''
+  },
+  {
+    time: '1PM',
+    text: ''
+  },
+  {
+    time: '2PM',
+    text: ''
+  },
+  {
+    time: '3PM',
+    text: ''
+  },
+  {
+    time: '4PM',
+    text: ''
+  },
+  {
+    time: '5PM',
+    text: ''
+  }
+];
 
-
-const updateHours = function() {
+const updateHourColors = function() {
   //get current local hour
   const currentHour = moment().local().format('k');
   
@@ -30,7 +69,36 @@ const updateHours = function() {
   return console.log("Hour colors updated");
 }
 
-updateHours();
+const updateHourData = function(event) {
+  const parentEl = $(event.target).closest('.row');
+  const hour = parentEl.children()[0].textContent;
+  const userText = parentEl.children()[1].value;
+  for (let index in hourData) {
+    if (hourData[index].time === hour) {
+      hourData[index].text = userText;
+    }
+  }
+  saveData();
+}
+
+const saveData = function() {
+  localStorage.setItem('data', JSON.stringify(hourData))
+  console.log("Data stored")
+}
+
+const loadData = function() {
+  const storedData = JSON.parse(localStorage.getItem('data'))
+  hourData = storedData
+  if (storedData) {
+    for (let index in storedData) {
+
+    }
+  }
+}
+
+updateHourColors();
 
 //update hour colors every minute
-setInterval(updateHours, (1000*60));
+setInterval(updateHourColors, (1000*60));
+
+containerEl.on('click', saveBtnEls, updateHourData);
